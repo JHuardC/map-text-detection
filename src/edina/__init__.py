1c6d5239d29e7dee5166310c91962cffa5b6726b
JHuardC <joehuard2711@gmail.com>
2026-07-02 02:34:38 +0100

    _geo_transforms.py - get_png_overlaps() function added.

--- src/edina/__init__.py
+++ src/edina/__init__.py
@@ -3,5 +3,6 @@ Edina module contains functions and classes used to extract and parse
 the geodata from tiff files downloaded from EDINA.
 """
 from ._parse_tab import parse_edina_tab_file
-from ._geo_transforms import get_transformer_from_geodataframe
+from ._geo_transforms import\
+    get_transformer_from_geodataframe, get_png_overlaps
 from ._tiffs_to_pngs import EDINATiffPNGConverter
--- src/edina/_geo_transforms.py
+++ src/edina/_geo_transforms.py
@@ -1,4 +1,4 @@
-from geopandas import GeoDataFrame
+from geopandas import GeoDataFrame, sjoin
 from rasterio.control import GroundControlPoint
 from rasterio.transform import GCPTransformer
 
@@ -39,3 +39,54 @@ def get_transformer_from_geodataframe(gdf: GeoDataFrame) -> GCPTransformer:
     )
     # Create GCPTransformer instance
     return GCPTransformer(gcps = gcps, tps = False)
+
+
+def get_png_overlaps(ctrl_points: GeoDataFrame) -> GeoDataFrame:
+    """
+    Combine PNG boundary shapes to retrieve the overlapping areas for
+    each PNG.
+
+    Parameter
+    ---------
+    ctrl_points: GeoDataFrame.
+        Required. GeoDataFrame containing the details of the control
+        points used for georeferencing. GeoDataFrame requires fields:
+        "geometry", "pixel_x", "pixel_y", "png_filename", and
+        "tiff_filename".
+    
+    Returns
+    -------
+    GeoDataFrame. Each record contains a polygon that represents the
+    overlap between each PNG. Includes the fields:
+    - tiff_filename: str. Filename for the TIFF that the PNGs were
+    derived from.
+    - png_filename_left, png_filename_right: str, str. Filenames for the
+    PNGs that the overlapping polygon geometry was derived from.
+    """
+    # Group control points by tiff_filename and png_filename
+    png_bounds = ctrl_points[["tiff_filename", "png_filename", "geometry"]]\
+        .dissolve(by = ["tiff_filename","png_filename"], as_index = False)
+    # Convert multipoint object to a polygon - polygon represents the
+    # area covered by the png
+    png_bounds["geometry"] = png_bounds.geometry.convex_hull
+
+    # Find which PNG areas overlap by joining PNGs with intersecting
+    # geometries
+    overlaps = png_bounds[["tiff_filename", "png_filename", "geometry"]]
+    overlaps = sjoin(
+        overlaps,
+        overlaps[["png_filename", "geometry"]],
+        how = "inner",
+        predicate = "intersects"
+    )
+    # Remove duplicate intersection pairs
+    overlaps = overlaps[overlaps.index < overlaps.index_right]
+    # Get overlapping space for each PNG pair
+    overlaps[ "geometry"] = overlaps[ "geometry"].intersection(
+        png_bounds.loc[overlaps.index_right, "geometry"], align = False
+    )
+    # Select inmportant fields
+    overlaps = overlaps[[
+        "tiff_filename", "png_filename_left", "png_filename_right", "geometry"
+    ]]
+    return overlaps
